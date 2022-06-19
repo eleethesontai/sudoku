190a878361f83fd914ceb6f3fe50a25fa5a2d80c
eleethesontai <eleethesontai@gmail.com>
2022-06-19 08:07:27 -0700

    converted text file to array of puzzles

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
+const fs = require("fs")
 const sudoku = require("./sudoku")
 
-// load puzzles from p096_sudoku.txt
-// loop puzzles and solve
+const data = fs.readFileSync("p096_sudoku.txt", "utf8");
+const results = data.match(/Grid \d{2}\n((\d{9}\n?)*)/gm)
 
+const puzzles = results.map(result => {
+    return result
+        .match(/\d{9}/g)
+        .map(x => {
+            return x.split('')
+        })
+})
+
+console.log(puzzles[0])
