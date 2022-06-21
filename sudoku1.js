e12e129822bcb372f0abbf603e6a0f89b310d726
eleethesontai <eleethesontai@gmail.com>
2022-06-21 09:04:35 -0700

    commit b4 debug.  it works but returns false? #wtf

--- sudoku1.js
+++ sudoku1.js
@@ -1,21 +1,22 @@
-function generate(puzzle) {
-    
-    for(row=0;row<9;row++) {
-        for(col=0;col<9;col++) {
+function solve(puzzle) {
 
-            if(puzzle[row][col]===0) {
+    for(r=0;r<9;r++) {
+        for(c=0;c<9;c++) {
+            
+            if(puzzle[r][c]===0) {
 
-                let random = r9()
-                random.forEach(n => {
-                    if(isValid(puzzle,n,row,col)) {
-                        puzzle[row][col] = n
-                        if(generate(puzzle)) {
+                for(n=1;n<9;n++) {
+                    if(isValid(puzzle,n,r,c)) {
+                        
+                        puzzle[r][c]=n
+                        if(solve(puzzle)) {
                             return true
                         } else {
-                            puzzle[row][col] = 0
+                            puzzle[r][c]=0 
                         }
+
                     }
-                })
+                }
 
                 return false
 
@@ -27,24 +28,23 @@ function generate(puzzle) {
     return true
 
 }
+function isValid(p,n,r,c) {
 
-function isValid(puzzle,number,row,col) {
-
-    let inRow = false
-    let inCol = false
-    let inBox = false
+    inRow = false
+    inCol = false
+    inBox = false
 
     for(i=0; i<9; i++) {
-        if(puzzle[row][i] === number) {inRow=true}
-        if(puzzle[i][col] === number) {inCol=true}
+        if(p[r][i]===n) {inRow=true}
+        if(p[i][c]===n) {inCol=true}
     }
 
-    let boxRow = row-row%3
-    let boxCol = col-col%3
+    boxRow=r-r%3
+    boxCol=c-c%3
 
-    for(r=boxRow;r<boxRow+3;r++) {
-        for(c=boxCol;c<boxCol+3;c++) {
-            if(puzzle[r][c] === number ) {inBox=true}
+    for(x=boxRow;x<boxRow+3;x++) {
+        for(y=boxCol;y<boxCol+3;y++) {
+            if(p[x][y]===n) {inBox=true}
         }
     }
 
@@ -91,12 +91,3 @@ function emptyPuzzle() {
         [0,0,0,0,0,0,0,0,0]
     ]
 }
-
-do {        
-    let puzzle = emptyPuzzle()
-    try {
-        result = generate(puzzle)
-    } catch(e) {
-        printPuzzle(puzzle)
-    }
-} while(1==1)
