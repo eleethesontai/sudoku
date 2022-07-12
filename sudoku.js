ce70a95d10111ccbd059c6c1bc012c3d9d2857e4
eleethesontai <eleethesontai@gmail.com>
2022-07-12 05:50:46 -0700

    wtf

--- sudoku.js
+++ sudoku.js
@@ -1,60 +1,48 @@
 const GRID_SIZE = 4
 
-function solve(grid) {
+function valid(grid,row,col,canidate) {
 
-    let isValid =(grid,row,col,num) => {
-        for(let i=0; i<4; i++) {
-            if (grid[row][i]===num) return false
-            if (grid[i][col]===num) return false
-        }
-
-        boxRow=row-row%2
-        boxCol=col-col%2
-
-        for(x=boxRow;x<boxRow+2;x++) {
-            for(y=boxCol;y<boxCol+2;y++) {
-                if(grid[x][y]===num) return false
-            }
-        }
-
-        return true
+    for(let i=0; i<GRID_SIZE; i++) {
+        if (grid[row][i]===canidate) return false
+        if (grid[i][col]===canidate) return false
     }
+
+    boxRow=row-row%2
+    boxCol=col-col%2
+
+    for(x=boxRow;x<boxRow+2;x++) {
+        for(y=boxCol;y<boxCol+2;y++) {
+            if(grid[x][y]===canidate) return false
+        }
+    }
+
+    return true
 
 }
 
-let result = solve([
-    [1,2,3,4],
+function solve(grid) {
+
+    let results = []
+    let stack = []
+
+    stack.push(grid)
+
+    while(stack.length>0) {
+
+        let current = stack.pop()
+
+    }
+
+    return results
+
+}
+
+let sample = [
+    [1,0,3,4],
     [4,3,2,1],
-    [0,0,0,0],
-    [0,0,0,0]
-])
+    [2,1,4,3],
+    [3,4,1,2]
+]
 
-/*
-
-1234
-4321
-2100
-0000
-
-*/
-
-/* 
-
-    1,
-    2,
-    3,
-    4,
-    4,
-    3,
-    2,
-    1,
-    0,
-        2,
-            1,
-
-            4
-        3,
-    0,
-
-
-*/
+let results = solve(sample)
+console.log(results)
