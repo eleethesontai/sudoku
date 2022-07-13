202152edea80d9faed4fa51a237a7a92aad010af
eleethesontai <eleethesontai@gmail.com>
2022-07-12 19:39:58 -0700

    cleaned

--- sudoku.js
+++ sudoku.js
@@ -1,62 +1,62 @@
-const GRID_SIZE = 4
-
 function valid(grid,index,canidate) {
 
-    // check column for canidate
-    for(let c=index%4; c<16; c+=4) {
-        if(grid[c]===canidate) return false
+    let houses = {
+        0: [0,1,2,3,0,4,8,12,0,1,4,5],
+        1: [0,1,2,3,1,5,9,13,0,1,4,5],
+        2: [0,1,2,3,2,6,10,14,2,3,6,7],
+        3: [0,1,2,3,3,7,11,15,2,3,6,7],
+        4: [4,5,6,7,0,4,8,12,0,1,4,5],
+        5: [4,5,6,7,1,5,9,13,0,1,4,5],
+        6: [4,5,6,7,2,6,10,14,2,3,6,7],
+        7: [4,5,6,7,3,7,11,15,2,3,6,7],
+        8: [8,9,10,11,0,4,8,12,8,9,12,13],
+        9: [8,9,10,11,1,5,9,13,8,9,12,13],
+        10: [8,9,10,11,2,6,10,14,10,11,14,15],
+        11: [8,9,10,11,3,7,11,15,10,11,14,15],
+        12: [12,13,14,15,0,4,8,12,8,9,12,13],
+        13: [12,13,14,15,1,5,9,13,8,9,12,13],
+        14: [12,13,14,15,2,6,10,14,10,11,14,15],
+        15: [12,13,14,15,3,7,11,15,10,11,14,15]
     }
 
-    // check row for canidate
-    for(let r=Math.floor(i/4)*4; r<16; r++) {
-        if(grid[r]===canidate) return false
-    }
-
-    return true
+    return !houses[index].map(e=>sample[e]).some(e=>e===canidate)
 
 }
 
-// function valid(grid,row,col,canidate) {
+function solve(grid) {
 
-//     for(let i=0; i<GRID_SIZE; i++) {
-//         if (grid[row][i]===canidate) return false
-//         if (grid[i][col]===canidate) return false
-//     }
+    let results = []
+    let stack = [grid]
 
-//     boxRow=row-row%2
-//     boxCol=col-col%2
+    while(stack.length>0) {
 
-//     for(x=boxRow;x<boxRow+2;x++) {
-//         for(y=boxCol;y<boxCol+2;y++) {
-//             if(grid[x][y]===canidate) return false
-//         }
-//     }
+        let current = stack.pop()
+        let index = current.indexOf(0)
 
-//     return true
+        if(index===-1) {
+            results.push(current)
+        } else {
+            for(let canidate=1; canidate<=4; canidate++) {
+                if(valid(current,index,canidate)) {
+                    current[index] = canidate
+                    stack.push(current) 
+                    current[index] = 0
+                }
+            }
+        }
 
-// }
+    }
 
-// function solve(grid) {
+    return results
 
-//     let results = []
-//     let stack = []
-
-//     stack.push(grid)
-
-//     return results
-
-// }
+}
 
 let sample = [
-    1,0,3,4,
+    1,2,3,4,
     4,3,2,1,
-    2,1,4,3,
-    3,4,1,2
+    3,4,0,2,
+    2,1,4,3
 ]
 
-// let results = solve(sample)
-// console.log(results)
-
-for(let i=0; i<16; i++) {
-    console.log(Math.floor(i/4)*4)
-}
+let results = solve(sample) 
+console.log(results)
