214d2a9ddabfdc8195669dc0ccb5e2f92698ad5f
eleethesontai <eleethesontai@gmail.com>
2022-07-16 08:39:12 -0700

    ported code i did at work

--- sudoku.js
+++ sudoku.js
@@ -1,66 +1,55 @@
+const GRID_SIZE = 4;
+ 
 function valid(grid,index,canidate) {
-
-    let houses = {
-        0: [0,1,2,3,0,4,8,12,0,1,4,5],
-        1: [0,1,2,3,1,5,9,13,0,1,4,5],
-        2: [0,1,2,3,2,6,10,14,2,3,6,7],
-        3: [0,1,2,3,3,7,11,15,2,3,6,7],
-        4: [4,5,6,7,0,4,8,12,0,1,4,5],
-        5: [4,5,6,7,1,5,9,13,0,1,4,5],
-        6: [4,5,6,7,2,6,10,14,2,3,6,7],
-        7: [4,5,6,7,3,7,11,15,2,3,6,7],
-        8: [8,9,10,11,0,4,8,12,8,9,12,13],
-        9: [8,9,10,11,1,5,9,13,8,9,12,13],
-        10: [8,9,10,11,2,6,10,14,10,11,14,15],
-        11: [8,9,10,11,3,7,11,15,10,11,14,15],
-        12: [12,13,14,15,0,4,8,12,8,9,12,13],
-        13: [12,13,14,15,1,5,9,13,8,9,12,13],
-        14: [12,13,14,15,2,6,10,14,10,11,14,15],
-        15: [12,13,14,15,3,7,11,15,10,11,14,15]
+ 
+    // check row
+    for(let r=Math.floor(index/GRID_SIZE)*GRID_SIZE; r<Math.floor(index/GRID_SIZE)*GRID_SIZE+GRID_SIZE; r++) {
+        if(grid[r]===canidate) return false
     }
-
-    return !houses[index].map(e=>sample[e]).some(e=>e===canidate)
-
+    // check col
+    for(let c=index%GRID_SIZE; c<GRID_SIZE**2; c+=4) {
+        if(grid[c]===canidate) return false
+    }
+ 
+    // check box
+ 
+    return true
+ 
 }
-
 function solve(grid) {
-
+ 
     let results = []
     let stack = [[...grid]]
-
-    console.log(stack[0]===grid)
-
+ 
     while(stack.length>0) {
-
+ 
         let current = stack.pop()
         let index = current.indexOf(0)
-
+ 
         if(index===-1) {
-            results.push(current)
+            results.push([...current])
         } else {
-            for(let canidate=1; canidate<=4; canidate++) {
+            for(let canidate=1; canidate<=GRID_SIZE; canidate++) {
                 if(valid(current,index,canidate)) {
                     current[index] = canidate
-                    stack.push([...current]) 
+                    stack.push([...current])
                     current[index] = 0
                 }
             }
         }
-
+ 
     }
-
+ 
     return results
-
+ 
 }
-
+ 
 let sample = [
-    4,2,1,3,
-    1,3,2,4,
+    4,0,0,0,
+    0,0,0,0,
     0,0,0,0,
     0,0,0,0
 ]
-
+ 
 let results = solve(sample)
-// results
-//     .forEach(r=>console.log(r.join('')))
-console.log(results.length)
+console.log(results)
