a09002e22e5f2fa0ce8b7ecee4bd5b3ad9533ea7
eleethesontai <eleethesontai@gmail.com>
2022-07-18 17:36:27 -0700

    done testing for the night

--- testing.js
+++ testing.js
@@ -1,3 +1,18 @@
 const sudoku = require("./sudoku")
 
-console.log(sudoku.generate(31))
+// let blank = 64
+// let sample = 100
+
+// let begin = new Date()
+
+// for(let i=0; i<sample; i++) {
+//     let puzzle = sudoku.generate(blank)
+// }
+
+// let end = new Date()
+// let duration = end-begin
+
+// console.log(duration, duration/sample)
+
+// let puzzle = sudoku.generate(32)
+// console.log(puzzle)
